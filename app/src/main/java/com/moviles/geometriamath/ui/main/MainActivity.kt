b76ab00aa2cd83337bfbdb0a1a32a9d100c27d93
davito19 <omardbonett@gmail.com>
2023-03-20 13:27:41 -0500

    Configuración del menu principal y creando menu desplegable

---
+++ app/src/main/java/com/moviles/geometriamath/ui/main/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.moviles.geometriamath.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.moviles.geometriamath.R
+import com.moviles.geometriamath.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+
+        setContentView(view)
+
+        val activities = resources.getStringArray(R.array.actividades)
+        val adapter = ArrayAdapter(this,R.layout.list_activities,activities)
+        with(mainBinding.autoCompleteTextView2){
+            setAdapter(adapter)
+            onItemClickListener = this@MainActivity
+        }
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val item = parent?.getItemAtPosition(position).toString()
+        Toast.makeText(this@MainActivity, item+" "+position, Toast.LENGTH_SHORT).show()
+        when(position){
+            0 -> TODO("areaCírculo")
+            1 -> TODO("Perímetro Círculo")
+            2 -> TODO("Área Cuadro")
+            3 -> TODO("Perímetro Cuadro")
+            4 -> TODO("Área Triángulo")
+            5 -> TODO("Perímetro Triángulo")
+            6 -> TODO("Área Rectángulo")
+            7 -> TODO("Perímetro Rectángulo")
+            8 -> TODO("Área de un Pentágono")
+            9 -> TODO("Perímetro de un Pentágono")
+            10 -> TODO("Área de un Hexágono")
+            11 -> TODO("Perímetro de un Hexágono")
+        }
+    }
+}
--- app/src/main/java/com/moviles/geometriamath/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/moviles/geometriamath/ui/splash/SplashActivity.kt
@@ -1,13 +1,15 @@
 package com.moviles.geometriamath.ui.splash
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.moviles.geometriamath.MainActivity
+import com.moviles.geometriamath.ui.main.MainActivity
 import com.moviles.geometriamath.databinding.ActivitySplashBinding
 import java.util.Timer
 import kotlin.concurrent.timerTask
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var splashBinding: ActivitySplashBinding
