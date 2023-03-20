c242919b034c6d87a9df2cd1ef91f1cd393dc0af
DANIEL MUNOZ <dafemure.180@gmail.com>
2023-03-20 14:45:55 -0500

    Se configura el boton de 'regresar' en la actvidad PerimetroTrianguloActivity.kt

--- app/src/main/java/com/moviles/geometriamath/ui/main/PerimetroTrianguloActivity.kt
+++ app/src/main/java/com/moviles/geometriamath/ui/main/PerimetroTrianguloActivity.kt
@@ -1,12 +1,24 @@
 package com.moviles.geometriamath.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.moviles.geometriamath.R
+import com.moviles.geometriamath.databinding.ActivityPerimetroTrianguloBinding
 
 class PerimetroTrianguloActivity : AppCompatActivity() {
+
+    private lateinit var perimetroTrianguloBinding: ActivityPerimetroTrianguloBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perimetro_triangulo)
+
+        perimetroTrianguloBinding = ActivityPerimetroTrianguloBinding.inflate(layoutInflater)
+
+        setContentView(perimetroTrianguloBinding.root)
+
+        perimetroTrianguloBinding.menuPpalButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
