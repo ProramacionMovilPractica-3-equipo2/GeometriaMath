a7ee11991cad16e5d6525be359e7238651c227a2
davito19 <omardbonett@gmail.com>
2023-03-20 11:55:04 -0500

    init project

---
+++ app/src/main/java/com/moviles/geometriamath/ui/splash/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.moviles.geometriamath.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
