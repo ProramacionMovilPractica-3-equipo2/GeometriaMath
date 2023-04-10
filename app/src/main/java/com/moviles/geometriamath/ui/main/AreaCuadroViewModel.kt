456e429bf9a2d245f475b58bbbffc3b216cc7d54
Yesika C <yesika.carvajal@udea.edu.co>
2023-04-10 00:20:08 -0500

    Finalizacion del area del perimetro

--- app/src/main/java/com/moviles/geometriamath/ui/main/AreaCuadroViewModel.kt
+++ app/src/main/java/com/moviles/geometriamath/ui/main/AreaCuadroViewModel.kt
@@ -1,4 +1,35 @@
 package com.moviles.geometriamath.ui.main
 
-class AreaCuadroViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AreaCuadroViewModel: ViewModel() {
+
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> = _error
+
+    val resultado: MutableLiveData<Float> by lazy{
+        MutableLiveData<Float>()
+    }
+
+
+    fun calcularArea(lado: String) {
+        if (lado.isEmpty()) {
+            _error.value = "Los campos no pueden estar vacíos."
+            return
+        }
+
+        else {
+            val valorLado = lado.toFloatOrNull()
+
+            if (valorLado == null || valorLado == 0f) {
+                _error.value = "Los valores ingresados no son válidos."
+                return
+            } else {
+                resultado.value = valorLado * valorLado
+            }
+        }
+    }
+
 }
