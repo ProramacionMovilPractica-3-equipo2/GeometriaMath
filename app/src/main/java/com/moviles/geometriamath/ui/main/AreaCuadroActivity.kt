4b7bf7afa11cfd3b2428a119884bc6f15b9856e2
Yesika C <yesika.carvajal@udea.edu.co>
2023-04-09 20:33:23 -0500

    Creación del activity

---
+++ app/src/main/java/com/moviles/geometriamath/ui/main/AreaCuadroActivity.kt
@@ -0,0 +1,12 @@
+package com.moviles.geometriamath.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.moviles.geometriamath.R
+
+class AreaCuadroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_area_cuadro)
+    }
+}
